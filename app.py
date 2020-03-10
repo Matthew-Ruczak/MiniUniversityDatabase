8383961c875c2c120ae733818f517b148e20989b
Matthew <matthewruczak@gmail.com>
2020-03-10 12:18:36 -0500

    Refactored Code.

--- app.py
+++ app.py
@@ -2,121 +2,122 @@ from type_of_person.employee import Employee
 from type_of_person.student import Student
 
 
-typesOfPersons = ("Employee", "Student")
 listOfPersons = []
 
 
-def addToDatabaseMenu():
+def add_person_menu():
     print("""
     What type of person would you like to add? (enter a number)
     1. Employee
     2. Student
     3. Exit
     """)
-    userInput = input('\t')
+    user_input = input('\t')
 
     # Processing user's input
-    if userInput == "1":
-        addPersonToDatabase(typesOfPersons[0])
-    elif userInput == "2":
-        addPersonToDatabase(typesOfPersons[1])
-    elif (userInput == "3"):
-        mainMenu()
+    if user_input == "1":
+        person_info_to_add_menu(Employee)
+    elif user_input == "2":
+        person_info_to_add_menu(Student)
+    elif user_input == "3":
+        main_menu()
     else:
-        print("Option not available, please try again")
-        addToDatabaseMenu()
-    mainMenu()
+        print("\tOption not available, please try again")
+        add_person_menu()
+    main_menu()
 
 
-def addPersonToDatabase(personType):
-    personFirstName = input('\tWhat is their first name? ')
-    personLastName = input('\tWhat is their last name? ')
-    if (personType == "Employee"):
-        personSpecialNum = input('What is their unique employee number?')
-        listOfPersons.append(Employee(personFirstName, personLastName, personSpecialNum))
-        print(f"Successfully Added {personLastName}")
-    else:
-        personSpecialNum = input('What is their unique student number?')
-        listOfPersons.append(Student(personFirstName, personLastName, personSpecialNum))
-        print(f"Successfully Added {personLastName}")
-    mainMenu()
+def person_info_to_add_menu(person_type):
+    person_first_name = input('\tWhat is their first name? ')
+    person_last_name = input('\tWhat is their last name? ')
+    newly_created_person = ""
+    if person_type == Employee:  # Creating an Employee
+        person_special_num = input('\tWhat is their unique employee number?')
+        newly_created_person = Employee(person_first_name, person_last_name, person_special_num)
+    else:   # Creating a Student
+        person_special_num = input('\tWhat is their unique student number?')
+        newly_created_person = Student(person_first_name, person_last_name, person_special_num)
+    listOfPersons.append(newly_created_person)
+    print(f"\tSuccessfully Added {person_first_name} {person_last_name}")
+    main_menu()
 
 
-def removePersonMenu():
+def remove_person_menu():
     # Processing User input
-    wasPersonRemoved = False
-    personToRemoveSpecialNum = input("What is their employee number? (OR enter 0000 to exit")
-    if personToRemoveSpecialNum == "0000":
-        mainMenu()
-    for person in listOfPersons:
-        if person.getEmployeeNum() == personToRemoveSpecialNum:
-            listOfPersons.remove(person)
-            wasPersonRemoved = True
-    if wasPersonRemoved:
-        print("Employee was removed")
+    person_to_remove_special_num = input("\tWhat is the person's special number? (OR enter 0000 to exit) ")
+    if person_to_remove_special_num == "0000":
+        main_menu()
+    # searching for person to remove
+    search_result = find_person_by_special_num(person_to_remove_special_num)
+    if search_result != -1:     # If a person was found
+        listOfPersons.remove(search_result)
+        print(f"\tPerson {person_to_remove_special_num} was removed")
+    else:   # Since a person was not found
+        print("\tEmployee not found, please try again.")
+        remove_person_menu()     # Letting user try again
+    main_menu()
+
+
+def update_person_menu():
+    print('\tUpdate Person Menu')
+    person_to_update_special_num = input("\tPlease enter their unique number or enter 0000 to exit: ")
+    if person_to_update_special_num == "0000":
+        main_menu()
+    person_to_update_obj = find_person_by_special_num(person_to_update_special_num)
+    if person_to_update_obj == -1:
+        print(f"\tUnique Number {person_to_update_special_num} was not found, please try again.")
+        update_person_menu()
     else:
-        print("Employee not found, please try again.")
-        removePersonMenu()
-    mainMenu()
+        person_to_update_menu(person_to_update_obj)
 
 
-def updatePersonMenu():
-    personToUpdateSpecialNum = input("\tPlease enter unique number of the person you would like to update? (OR enter 0000 to exit)")
-    if personToUpdateSpecialNum == "0000":
-        mainMenu()
-    personToUpdateObj = findPersonByNumber(personToUpdateSpecialNum)
-    if personToUpdateObj == -1:
-        print(f"Unique Number {personToUpdateSpecialNum} was not found, please try again.")
-        updatePersonMenu()
-    else:
-        personToUpdateMenu(personToUpdateObj)
-
-
-def personToUpdateMenu(personToUpdateObj):
-    print(f"What would you like to update for {personToUpdateObj[0].getFullName()}?")
-    print("""1. First Name
-             2. Last Name
-             3. Unique Number
-             4. Exit
+def person_to_update_menu(person_to_update_obj):
+    print(f"\tWhat would you like to update for {person_to_update_obj.getFullName()}?")
+    print("""
+        1. First Name
+         2. Last Name
+         3. Unique Number
+         4. Exit
     """)
-    userSelectedFieldToUpdate = input()
+    field_to_update = input()
 
-    if userSelectedFieldToUpdate == "1":
-        newPersonFirstName = input("Please enter their new first name: ")
-        personToUpdateObj[0].setFirstName(newPersonFirstName)
-    elif userSelectedFieldToUpdate == "2":
-        newPersonLastName = input("Please enter their new first name: ")
-        personToUpdateObj[0].setLastName(newPersonLastName)
-    elif userSelectedFieldToUpdate == "3":
-        if personToUpdateObj[1] == Employee:
-            newEmployeeNumber = input("What is their new Employee #")
-            personToUpdateObj[0].setEmployeeNum(newEmployeeNumber)
-        elif personToUpdateObj[1] == Student:
-            newStudentNumber = input("What is their new Student #")
-            personToUpdateObj[0].setStudentNumber(newStudentNumber)
-    elif userSelectedFieldToUpdate == "4":
-        mainMenu()
+    if field_to_update == "1":
+        new_person_first_name = input("\tPlease enter their new first name: ")
+        person_to_update_obj.setFirstName(new_person_first_name)
+    elif field_to_update == "2":
+        new_person_last_name = input("\tPlease enter their new first name: ")
+        person_to_update_obj.setLastName(new_person_last_name)
+    elif field_to_update == "3":
+        # Updating Person's special number (depending on what type of Person they are)
+        if type(person_to_update_obj) == Employee:
+            person_to_update_obj.setEmployeeNum(input("\tWhat is their new Employee #"))
+        elif type(person_to_update_obj) == Student:
+            person_to_update_obj.setStudentNumber(input("\tWhat is their new Student #"))
+    elif field_to_update == "4":
+        main_menu()
     else:
-        print("Invalid input, please try again")
-    personToUpdateMenu(personToUpdateObj)
+        print("\tInvalid input, please try again")
+    person_to_update_menu(person_to_update_obj)  # Let's the user update another field or try again
 
-def searchForPersonByIDMenu():
-    searchForNum = input("What is their special number? (OR enter 0000 to exit) ")
-    if searchForNum == "0000":
-        mainMenu()
-    searchResult = findPersonByNumber(searchForNum)
-    if searchResult == -1:
-        print(f'No person found with the special number {searchForNum}, please try again.')
+
+def search_for_person_by_id_menu():
+    search_for_special_num = input("\tWhat is their special number? (OR enter 0000 to exit) ")
+    if search_for_special_num == "0000":
+        main_menu()
+    search_result = find_person_by_special_num(search_for_special_num)
+    if search_result == -1:
+        print(f'\tNo person found with the special number {search_for_special_num}, please try again.')
     else:
-        if type(searchResult[0]) == Employee:
-            print(f"Employee {searchResult[0].getFullName()} {searchResult[0].getEmployeeNum()}")
-        elif type(searchResult[0]) == Student:
-            print(f"Student {searchResult[0].getFullName()} {searchResult[0].getStudentNumber()}")
+        if type(search_result[0]) == Employee:
+            print(f"\tEmployee {search_result[0].getFullName()} {search_result[0].getEmployeeNum()}")
+        elif type(search_result[0]) == Student:
+            print(f"\tStudent {search_result[0].getFullName()} {search_result[0].getStudentNumber()}")
         else:
-            print(f"Error when searching for {searchForNum}, please try again")
-    searchForPersonByIDMenu()
+            print(f"\tError when searching for {search_for_special_num}, please try again")
+    search_for_person_by_id_menu()
 
-def displayAllEmployeesAndStudent():
+
+def display_all_persons():
     print("\t_______________________")
     print("\t\tPersons")
     print("\t-----------------------")
@@ -126,12 +127,12 @@ def displayAllEmployeesAndStudent():
         elif type(person) == Student:
             print(f"\tStudent {person.getFullName()} {person.getStudentNumber()}")
         else:
-            print(f"Error with Person, data was not retrieved.")
+            print(f"\tError with Person, data was not retrieved.")
     print("\t_______________________")
-    mainMenu()
+    main_menu()
 
 
-def mainMenu():
+def main_menu():
     print("""
     Welcome to Mini University Database
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -143,34 +144,36 @@ def mainMenu():
     5. View All
     6. Exit
     ---------------------------------------""")
-    userInput = input('\t')
+    selected_option = input('\t')
 
-    #Processing user's input
-    if userInput == "1":
-        addToDatabaseMenu()
-    elif userInput == "2":
-        removePersonMenu()
-    elif userInput == "3":
-        updatePersonMenu()
-    elif userInput == "4":
-        searchForPersonByIDMenu()
-    elif userInput == "5":
-        displayAllEmployeesAndStudent()
-    elif userInput == "6":
-        print("Please close the program using the exit button in the window")
+    # Processing user's input
+    if selected_option == "1":
+        add_person_menu()
+    elif selected_option == "2":
+        remove_person_menu()
+    elif selected_option == "3":
+        update_person_menu()
+    elif selected_option == "4":
+        search_for_person_by_id_menu()
+    elif selected_option == "5":
+        display_all_persons()
+    elif selected_option == "6":
+        exit(0)
     else:
-        print("Invalid input, please enter a number from the above list. (Without a period)")
-    mainMenu()
+        print("\tInvalid input, please enter a number from the above list. (Without a period)")
+    main_menu()
 
-def findPersonByNumber(unqiueNum):
+
+def find_person_by_special_num(special_num):
     for person in listOfPersons:
         if type(person) == Employee:
-            if person.getEmployeeNum() == unqiueNum:
-                return person, Employee
+            if person.getEmployeeNum() == special_num:
+                return person
         if type(person) == Student:
-            if person.getStudentNumber() == unqiueNum:
-                return person, Student
+            if person.getStudentNumber() == special_num:
+                return person
     return -1   # No person was found
 
+
 # Displaying the main menu when the user first enters the program
-mainMenu()
+main_menu()
